4888cf93d57dd85f7bfc081b722b0237b93cc9ed
Alan <alan.t.cumiskey@gmail.com>
2014-09-17 23:07:37 +0100

    Rechecked

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140916134137) do
+ActiveRecord::Schema.define(version: 20140917215617) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
