7e9a6e9268307a3f7a67187158d9367e1b6db5f9
Alan <alan.t.cumiskey@gmail.com>
2014-09-23 18:49:31 +0100

    smtp inclusion

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'bootstrap-sass'
 gem 'devise', '~> 3.3.0'
+gem 'sendgrid-ruby'
 
 group :development, :test  do 
 		gem 'sqlite3'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :pins
 
-  devise_for :users
   root 'pages#home'
   get 'about' => 'pages#about'
   get 'contact' => 'pages#contact'
