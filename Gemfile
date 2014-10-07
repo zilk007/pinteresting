73d8a746f27427f16a435f359aba67085dfd71a1
Alan <alan.t.cumiskey@gmail.com>
2014-10-07 04:03:54 +0100

    add amazon s3

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'bootstrap-sass'
 gem 'devise', '~> 3.3.0'
 gem 'paperclip', '~> 3.0'
-#gem 'paperclip', github: 'thoughtbot/paperclip'
+gem 'aws-sdk', '~> 1.20.0'
 
 
 group :development, :test  do 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,5 +95,14 @@ Rails.application.configure do
   :domain         => ENV['SENDGRID_DOMAIN']
 }
 
+# sets papaerclip to upload images to amazon s3
+config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 
 end
